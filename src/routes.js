0f4b24835211058d375229d9e6b03a4ed9dfd558
educhagas00 <educhagas00@gmail.com>
2025-02-01 16:14:05 -0300

    feat: CRUD for categories

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ const { Router } = require('express'); // desestruturação do express para pega
 const router = Router();
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
+router.get('/categories/:id', CategoryController.show);
+router.delete('/categories/:id', CategoryController.delete);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
